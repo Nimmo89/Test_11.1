bdd2c6c15fe7aea3f0e087aa162b7851952bb1ba
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-21 22:41:15 +0700

    Update ProductRepository.java

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,25 +1,33 @@
 package ru.netology.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.netology.domain.Product;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ProductRepository {
-    private Product[] items = new Product[0];
+    private static Product[] products = new Product[0];
+    private int id;
+
 
     public void save(Product item) {
-        int length = items.length + 1;
+        int length = products.length + 1;
         Product[] tmp = new Product[length];
-        System.arraycopy(items, 0, tmp, 0, items.length);
+        System.arraycopy(products, 0, tmp, 0, products.length);
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
-        items = tmp;
+        products = tmp;
     }
 
-    public Product[] findAll() {
-        return items;
+    public static Product[] findAll() {
+        return products;
     }
 
     public Product findById(int id) {
-        for (Product item : items) {
+        for (Product item : products) {
             if (item.getId() == id) {
                 return item;
             }
@@ -28,15 +36,15 @@ public class ProductRepository {
     }
 
     public void removeById(int id) {
-        int length = items.length - 1;
+        int length = products.length - 1;
         Product[] tmp = new Product[length];
         int index = 0;
-        for (Product item : items) {
+        for (Product item : products) {
             if (item.getId() != id) {
                 tmp[index] = item;
                 index++;
             }
         }
-        items = tmp;
+        products = tmp;
     }
 }
