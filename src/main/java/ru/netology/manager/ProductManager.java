eb0b4f1ca0fcfe2116c81fe7f13b0689f396bd80
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-24 13:59:28 +0700

    Update ProductManager.java

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -6,36 +6,37 @@ import ru.netology.repository.ProductRepository;
 public class ProductManager {
     private ProductRepository[] products = new ProductRepository[0];
 
-    public void add(ProductRepository product) {
+    public void add(Product product) {
+        products.
         int length = products.length + 1; //mew massive + 1
         ProductRepository[] tmp = new ProductRepository[length];
         System.arraycopy(products, 0, tmp, 0, products.length); //copy all elements
         int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = product;
+        tmp[lastIndex] =;
         products = tmp;
     }
 
-    public ProductRepository[] getAll() {
-        ProductRepository[] result = new ProductRepository[products.length];
-        for (int i = 0; i < result.length; i++) {
-            int index = products.length - 1 - i;
-            result[i] = products[index];
-        }
-        return result;
-    }
-
-    public void removeById(int id) {
-        int length = products.length - 1;
-        ProductRepository[] tmp = new ProductRepository[length];
-        int index = 0;
-        for (ProductRepository product : products) {
-            if (product.getId() != id) {
-                tmp[index] = product;
-                index++;
-            }
-        }
-        products = tmp;
-    }
+//    public ProductRepository[] getAll() {
+//        ProductRepository[] result = new ProductRepository[products.length];
+//        for (int i = 0; i < result.length; i++) {
+//            int index = products.length - 1 - i;
+//            result[i] = products[index];
+//        }
+//        return result;
+//    }
+//
+//    public void removeById(int id) {
+//        int length = products.length - 1;
+//        ProductRepository[] tmp = new ProductRepository[length];
+//        int index = 0;
+//        for (ProductRepository product : products) {
+//            if (product.getId() != id) {
+//                tmp[index] = product;
+//                index++;
+//            }
+//        }
+//        products = tmp;
+//    }
 
     public boolean matches(Product product, String search) {
         if (product instanceof Book) { // если в параметре product лежит объект класса Book
@@ -43,21 +44,21 @@ public class ProductManager {
             if (book.getAuthor().contains(search)) { // проверим есть ли поисковое слово в данных об авторе
                 return true;
             }
-                return false;
+            return false;
+        }
+        if (product instanceof Smartphone) { // если в параметре product лежит объект класса Smartphone
+            Smartphone smartphone = (Smartphone) product; // положем его в переменную типа Smartphone чтобы пользоваться методами класса Smartphone
+            if (smartphone.getProducer().contains(search)) { // проверим есть ли поисковое слово в данных об производителе
+                return true;
             }
-            if (product instanceof Smartphone) { // если в параметре product лежит объект класса Smartphone
-                Smartphone smartphone = (Smartphone) product; // положем его в переменную типа Smartphone чтобы пользоваться методами класса Smartphone
-                if (smartphone.getProducer().contains(search)) { // проверим есть ли поисковое слово в данных об производителе
-                    return true;
-                }
-                    return false;
-                }
+            return false;
+        }
         return false;
     }
 
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
-        for (Product product: ProductRepository.findAll()) {
+        for (Product product : ProductRepository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
                 System.arraycopy(products, 0, tmp, 0, products.length);
@@ -66,5 +67,9 @@ public class ProductManager {
             }
         }
         return result;
+    }
+
+    public ProductManager(ProductRepository[] products) {
+        this.products = products;
     }
 }
