88fe076da31be86a72d9564cd7228d6c145dbe69
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-24 14:57:51 +0700

    Update ProductManager.java

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -4,39 +4,11 @@ import ru.netology.domain.*;
 import ru.netology.repository.ProductRepository;
 
 public class ProductManager {
-    private ProductRepository[] products = new ProductRepository[0];
+    private ProductRepository repository;
 
     public void add(Product product) {
-        repos
-        int length = products.length + 1; //mew massive + 1
-        ProductRepository[] tmp = new ProductRepository[length];
-        System.arraycopy(products, 0, tmp, 0, products.length); //copy all elements
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] =;
-        products = tmp;
+        repository.save(product);
     }
-
-//    public ProductRepository[] getAll() {
-//        ProductRepository[] result = new ProductRepository[products.length];
-//        for (int i = 0; i < result.length; i++) {
-//            int index = products.length - 1 - i;
-//            result[i] = products[index];
-//        }
-//        return result;
-//    }
-//
-//    public void removeById(int id) {
-//        int length = products.length - 1;
-//        ProductRepository[] tmp = new ProductRepository[length];
-//        int index = 0;
-//        for (ProductRepository product : products) {
-//            if (product.getId() != id) {
-//                tmp[index] = product;
-//                index++;
-//            }
-//        }
-//        products = tmp;
-//    }
 
     public boolean matches(Product product, String search) {
         if (product instanceof Book) { // если в параметре product лежит объект класса Book
@@ -44,11 +16,17 @@ public class ProductManager {
             if (book.getAuthor().contains(search)) { // проверим есть ли поисковое слово в данных об авторе
                 return true;
             }
+            if (book.getName().contains(search)) {
+                return true;
+            }
             return false;
         }
         if (product instanceof Smartphone) { // если в параметре product лежит объект класса Smartphone
             Smartphone smartphone = (Smartphone) product; // положем его в переменную типа Smartphone чтобы пользоваться методами класса Smartphone
             if (smartphone.getProducer().contains(search)) { // проверим есть ли поисковое слово в данных об производителе
+                return true;
+            }
+            if (smartphone.getName().contains(search)) {
                 return true;
             }
             return false;
@@ -61,7 +39,7 @@ public class ProductManager {
         for (Product product : ProductRepository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
-                System.arraycopy(products, 0, tmp, 0, products.length);
+                System.arraycopy(repository, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = product;
                 result = tmp;
             }
@@ -69,7 +47,7 @@ public class ProductManager {
         return result;
     }
 
-    public ProductManager(ProductRepository[] products) {
-        this.products = products;
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
     }
 }
