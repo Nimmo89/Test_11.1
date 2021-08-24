62fda459d647e32f19b1a8bd7984043c21b8294c
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-25 00:45:33 +0700

    Update ProductManagerTest.java

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -2,8 +2,7 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.netology.domain.Book;
-import ru.netology.domain.Smartphone;
+import ru.netology.domain.*;
 import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
