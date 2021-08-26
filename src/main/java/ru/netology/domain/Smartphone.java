d2c62c4f7a94cf800b903f18efecd2fad75ff296
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-26 19:13:20 +0700

    Update Smartphone.java

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -22,24 +22,24 @@ public class Smartphone extends Product {
 //        this.producer = producer;
 //    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Smartphone that = (Smartphone) o;
-        return Objects.equals(producer, that.producer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), producer);
-    }
-
-    @Override
-    public String toString() {
-        return "Smartphone{" +
-                "producer='" + producer + '\'' +
-                '}';
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        if (!super.equals(o)) return false;
+//        Smartphone that = (Smartphone) o;
+//        return Objects.equals(producer, that.producer);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(super.hashCode(), producer);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Smartphone{" +
+//                "producer='" + producer + '\'' +
+//                '}';
+//    }
 }
