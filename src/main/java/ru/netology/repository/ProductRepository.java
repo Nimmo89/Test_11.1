57f6dde0034f5876d5acdefdd9f7098254ae4542
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-27 00:10:39 +0700

    Update ProductRepository.java

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -6,7 +6,6 @@ public class ProductRepository {
     private Product[] products = new Product[0];
 
     public void save(Product item) {
-//        int length = products.length + 1;
         Product[] tmp = new Product[products.length + 1];
 //        System.arraycopy(products, 0, tmp, 0, products.length);
         for (int i = 0; i < products.length; i++) {
