163fc719c6fb23dfe517b4f86f1508bb49f9cb83
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-27 07:47:25 +0700

    Update Smartphone.java

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 public class Smartphone extends Product {
     private String producer;
 
-//    public Smartphone() {
-//        super();
-//    }
+    public Smartphone() {
+        super();
+    }
 
     public Smartphone(int id, String name, int price, String producer) {
         super(id, name, price);
@@ -18,28 +18,28 @@ public class Smartphone extends Product {
         return producer;
     }
 
-//    public void setProducer(String producer) {
-//        this.producer = producer;
-//    }
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        if (!super.equals(o)) return false;
-//        Smartphone that = (Smartphone) o;
-//        return Objects.equals(producer, that.producer);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(super.hashCode(), producer);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Smartphone{" +
-//                "producer='" + producer + '\'' +
-//                '}';
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Smartphone that = (Smartphone) o;
+        return Objects.equals(producer, that.producer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), producer);
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "producer='" + producer + '\'' +
+                '}';
+    }
 }
